9c59beb00e9069967002aad6c174c57b05f253e1
Brock <3rb@ornl.gov>
2021-12-21 17:04:38 -0600

    reconfigured routing and completed app bar for site

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+  {
+    path:'customers',
+    loadChildren:  () => import("./customers/customers.module").then(mod => mod.CustomersModule)
+  },
+  {
+    path:'orders',
+    loadChildren:  () => import('./orders/orders.module').then(mod => mod.OrdersModule)
+  },
+  {
+    path:'messages',
+    loadChildren: () => import('./messages/messages.module').then(mod => mod.MessagesModule)
+  },
+  {
+    path:'',
+    redirectTo:'',
+    pathMatch: 'full'
+  }
+]
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
