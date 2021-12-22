060bdd867c09cd4b5f3193e797ac30ac31752b8c
Brock <3rb@ornl.gov>
2021-12-22 14:23:37 -0600

    started working on setting up navigation to the messages

--- src/app/messages/messages.module.ts
+++ src/app/messages/messages.module.ts
@@ -1,17 +1,40 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule, FormControlName } from '@angular/forms';
 
 import { MessagesRoutingModule } from './messages-routing.module';
 import { MessageListComponent } from './message-list/message-list.component';
+import { MessageNewComponent } from './message-new/message-new.component';
+
+//Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatIconModule } from '@angular/material/icon';
+import { MatAutocompleteModule } from '@angular/material/autocomplete'
+
 
 
 @NgModule({
   declarations: [
-    MessageListComponent
+    MessageListComponent,
+    MessageNewComponent
   ],
   imports: [
     CommonModule,
-    MessagesRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    FormControlName,
+    MessagesRoutingModule,
+
+    ///Material
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatStepperModule,
+    MatIconModule,
+    MatAutocompleteModule
   ]
 })
 export class MessagesModule { }
